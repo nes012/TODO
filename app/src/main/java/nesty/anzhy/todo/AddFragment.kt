9304bb7e056cdeb4d8c62253a6c67aa3ef2b802c
nes012 <anzhy.nesty@gmail.com>
2021-10-03 16:11:07 +0300

    Initial commit

---
+++ app/src/main/java/nesty/anzhy/todo/AddFragment.kt
@@ -0,0 +1,17 @@
+package nesty.anzhy.todo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AddFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
